e39d52453821533640c27e9b41d7faf8e0421709
JuhoR <60245517+DeveloperJuho@users.noreply.github.com>
2023-11-15 11:17:00 +0200

    Etch-A-Sketch complete with extras

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 // Global variables
 
+let selectedMode = "RGB";
+let incrementAmount = 1;
 
 // wait for DOM to load
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Div for squares
     const squareContainer = document.querySelector(".sqrContainer");
     const resetButton = document.querySelector(".sqrButton");
+    const normalButton = document.querySelector(".normalMode");
+    const rgbButton = document.querySelector(".rgbMode");
+    const incrementButton = document.querySelector(".blackMode");
 
+    // Mode buttons, every mode resets blackening functions number
+    normalButton.addEventListener("click", () => {
+        incrementAmount = 1;
+        selectedMode = "Normal";
+    });
+
+    rgbButton.addEventListener("click", () => {
+        incrementAmount = 1;
+        selectedMode = "RGB";
+    });
+
+    incrementButton.addEventListener("click", () => {
+        incrementAmount = 1;
+        selectedMode = "Increment";
+    })
+
+    // Reset button
     resetButton.addEventListener("click", () =>{
         const inputField = document.querySelector(".sqrInput");
 
         if(Number(inputField.value) <= 64 && Number(inputField.value > 0)){
             createGrid(squareContainer, inputField.value);
+            incrementAmount = 1;
         }     
         else{
             createGrid(squareContainer, 16);
+            incrementAmount = 1;
         }   
     });
 
@@ -28,10 +51,8 @@ document.addEventListener("DOMContentLoaded", () => {
 const squareConstructor = function(){
     const basicSquare = document.createElement("div");
     basicSquare.className = "squareDiv";
-
-    basicSquare.addEventListener("mouseenter", () => {
-        basicSquare.classList.add("painted");
-    });
+    
+    basicSquare.addEventListener("mouseover", () => paintMode(basicSquare));
 
     return basicSquare;
 }
@@ -54,5 +75,24 @@ const createGrid = function(sqrContainer, divNumber){
 
     for(let i = 0; i < divNumber; i++){
         sqrContainer.appendChild(rowConstructor(divNumber));
+    }
+}
+
+// Painting function
+
+const paintMode = (square) => {
+    if(selectedMode == "Normal"){
+        square.style.backgroundColor = "black";
+    }
+
+    else if(selectedMode == "RGB"){
+        square.style.backgroundColor = `rgb(${(Math.floor(Math.random()*255))},${(Math.floor(Math.random()*255))},${(Math.floor(Math.random()*255))})`;
+    }
+
+    else if(selectedMode == "Increment"){
+        if(incrementAmount >= 0.1){
+            square.style.backgroundColor = `rgb(${incrementAmount*255},${incrementAmount*255},${incrementAmount*255})`;
+        }
+        incrementAmount -= 0.1;
     }
 }
